c3f29eeb6c323adca539296bd41cf765c25a03d8
Anderson <anderson.prante@gmail.com>
2022-02-26 11:51:45 -0300

    first

---
+++ precache-manifest.293de08253fb177328dbfae0c6bb0434.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c5ef7dba80369b8b57b",
+    "url": "/hello/css/app.17d0086d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/hello/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "7ccacf6541ff3c81381fda5c57893393",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "415cc0359920c071533e",
+    "url": "/hello/js/about.62743e6c.js"
+  },
+  {
+    "revision": "9c5ef7dba80369b8b57b",
+    "url": "/hello/js/app.725f11d3.js"
+  },
+  {
+    "revision": "79ba59c9038d8ff16f53",
+    "url": "/hello/js/chunk-vendors.cbe2ed46.js"
+  },
+  {
+    "revision": "92c481f3ee3a3507e71a9616a7ff91ac",
+    "url": "/hello/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/hello/robots.txt"
+  }
+]);
---
+++ precache-manifest.6260e13a503a50cc00cc2a1f26cdfd23.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5759bbe0d54ff7aed8ce",
+    "url": "/a/css/app.17d0086d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/a/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "44fb8e907baf53f901fdb684f945ea89",
+    "url": "/a/index.html"
+  },
+  {
+    "revision": "415cc0359920c071533e",
+    "url": "/a/js/about.62743e6c.js"
+  },
+  {
+    "revision": "5759bbe0d54ff7aed8ce",
+    "url": "/a/js/app.e891ddcf.js"
+  },
+  {
+    "revision": "79ba59c9038d8ff16f53",
+    "url": "/a/js/chunk-vendors.cbe2ed46.js"
+  },
+  {
+    "revision": "92c481f3ee3a3507e71a9616a7ff91ac",
+    "url": "/a/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/a/robots.txt"
+  }
+]);
